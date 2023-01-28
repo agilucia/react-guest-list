84ccfce5dbf882a40f1e4ae4d6e09feafb16fd2b
Agnes Pilar <agnes.pilar@yahoo.de>
2023-01-28 21:33:15 +0100

    Update prettier in VSCode

--- src/ListApi.js
+++ src/ListApi.js
@@ -13,17 +13,17 @@ export default function ListApi() {
   const baseUrl = 'http://localhost:4000';
 
   async function fetchGuests() {
-      const response = await fetch(`${baseUrl}/guests`);
-      const allGuests = await response.json();
-      setGuest(allGuests);
-      setIsLoading(false);
-    }
-    useEffect(() => {
+    const response = await fetch(`${baseUrl}/guests`);
+    const allGuests = await response.json();
+    setGuest(allGuests);
+    setIsLoading(false);
+  }
+  useEffect(() => {
     fetchGuests().catch(() => console.log('fetching guests went wrong'));
   }, []);
 
   // if (isLoading) {
- //    return 'is Loading...';
+  //    return 'is Loading...';
   // }
 
   // adding a new guest to the list
@@ -51,13 +51,15 @@ export default function ListApi() {
 
   // deleting a guest from the list
   async function deleteGuest(id) {
-    const response = await fetch(`${baseUrl}/guests/${id}`, { method: 'DELETE', });
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'DELETE',
+    });
     const deletedGuest = await response.json();
     const newGuestList = guest.filter((i) => {
       return i.id !== deletedGuest.id;
     });
     setGuest(newGuestList);
-    fetchGuests().catch(() => console.log('deleting guest went wrong'),);
+    fetchGuests().catch(() => console.log('deleting guest went wrong'));
   }
 
   // attending/not attending status
@@ -74,76 +76,89 @@ export default function ListApi() {
       return i.id !== updatedGuest.id;
     });
     setGuest([...guest], updatedGuestList);
-    fetchGuests().catch(() => console.log('changing attendance status went wrong'),);
+    fetchGuests().catch(() =>
+      console.log('changing attendance status went wrong'),
+    );
   }
 
   return isLoading ? (
-    <h1>Loading...</h1> ) : (
+    <h1>Loading...</h1>
+  ) : (
     <div data-test-id="guest">
       <h1>Guest List</h1>
       <div>
-      <div>
         <div>
-          <h2>Add new Guest:</h2>
-          <form name="form" onSubmit={addNewGuest}>
-            <label>First name
-              <input value={firstName}
-              onChange={(event) => setFirstName(event.currentTarget.value)} />
-            </label>
-            <br />
-            <label>Last name
-              <input value={lastName}
-              onChange={(event) => setLastName(event.currentTarget.value)} />
-            </label>
-            <br />
-            <div>
-              <button onClick={addNewGuest}>ADD GUEST</button>
-            </div>
-          </form>
-        </div>
-      </div>
-      <div>
-        {guest.length === 0 ? (
           <div>
-            Add guests to the list!
+            <h2>Add new Guest:</h2>
+            <form name="form" onSubmit={addNewGuest}>
+              <label>
+                First name
+                <input
+                  value={firstName}
+                  onChange={(event) => setFirstName(event.currentTarget.value)}
+                />
+              </label>
+              <br />
+              <label>
+                Last name
+                <input
+                  value={lastName}
+                  onChange={(event) => setLastName(event.currentTarget.value)}
+                />
+              </label>
+              <br />
+              <div>
+                <button onClick={addNewGuest}>ADD GUEST</button>
+              </div>
+            </form>
           </div>
-        ): (
+        </div>
         <div>
-          <h2>Guests:</h2>
-          {guest.map((guestList) => {
-            return (
-              <div key={guestList.id}>
+          {guest.length === 0 ? (
+            <div>Add guests to the list!</div>
+          ) : (
+            <div>
+              <h2>Guests:</h2>
+              {guest.map((guestList) => {
+                return (
+                  <div key={guestList.id}>
                     <div>
                       {guestList.firstName} {guestList.lastName}
                     </div>
                     <label>
-                      <input aria-label={`attending status ${guestList.firstName} ${guestList.lastName}`}
-                      type="checkbox"
-                      checked={guestList.attending}
-                      onChange={() => {
-                        changeAttendanceStatus(
-                          guestList.id,
-                          guestList.attending,
-                        ).catch((error) =>
-                        console.log(error),);
-                      }} />
-                      {guestList.attending === true ? 'attending' : 'not attending'}
+                      <input
+                        aria-label={`attending status ${guestList.firstName} ${guestList.lastName}`}
+                        type="checkbox"
+                        checked={guestList.attending}
+                        onChange={() => {
+                          changeAttendanceStatus(
+                            guestList.id,
+                            guestList.attending,
+                          ).catch((error) => console.log(error));
+                        }}
+                      />
+                      {guestList.attending === true
+                        ? 'attending'
+                        : 'not attending'}
                     </label>
-                    <button aria-label={`Remove ${guestList.firstName} ${guestList.lastName}`}
-                    onClick={() => {
-                      deleteGuest(guestList.id).catch((error) =>
-                      console.log(error),);
-                      document.form.reset();
-                    }}>
+                    <button
+                      aria-label={`Remove ${guestList.firstName} ${guestList.lastName}`}
+                      onClick={() => {
+                        deleteGuest(guestList.id).catch((error) =>
+                          console.log(error),
+                        );
+                        document.form.reset();
+                      }}
+                    >
                       x
-                      </button>
-              </div>
-            );
-          })}
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
-        )}
       </div>
     </div>
-  </div>
   );
 }
